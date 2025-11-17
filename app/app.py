e7e2fefd27a9f6dc601fe4bc8832adcdb0c46c62
LulukDWiKuncoro <lulu23203ti@student.nurulfikri.ac.id>
2025-11-17 14:09:33 +0700

    Initial commit of all project files

---
+++ app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template_string
+
+app = Flask(__name__)
+
+HTML = """
+<!doctype html>
+<title>My Cloud App</title>
+<h1>Ujian BNSP</h1>
+<p>Semoga saya lulus</p>
+<p>yang diperlukan adalah: Docker + AWS EC2 + CI/CD + Prometheus + Grafana</p>
+"""
+
+@app.route("/")
+def index():
+    return render_template_string(HTML)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
